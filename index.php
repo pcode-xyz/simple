d96d5717776396a2584380e8e1e516eeaf0a09b9
gavinczhang <gavin6487@gmail.com>
2016-04-20 23:39:50 +0800

    * 回退

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ date_default_timezone_set('Asia/Shanghai');
 set_time_limit(0);
 
 //定义路径
-define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
+define('DOCROOT', __DIR__.'/');
 
 //自动挂载
-require_once DOCROOT.'class'.DIRECTORY_SEPARATOR.'core.php';
+require_once DOCROOT.'class/core.php';
 spl_autoload_register(array('Core', 'auto_load'));
 
 //定义route并启动
